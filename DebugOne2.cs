f19189c45ec6e01a56c8a3e9e4aa86a6c9865ee0
chagow <tshadmoua@gmail.com>
2025-01-30 14:32:51 -0600

    Create DebugOne2.cs

---
+++ DebugOne2.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+using System.Globalization;
+class DebugOne2
+{
+   static void Main()
+   {
+      Console.WriteLine("This program displays a square");
+      Console.WriteLine("&&&&&&&&&&");
+      Console.WriteLine("&        &");
+      Console.WriteLine("&        &");
+      Console.WriteLine("&        &");
+      Console.WriteLine("&&&&&&&&&&");
+}
+}
