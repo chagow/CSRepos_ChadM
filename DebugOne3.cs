bb3b05eb22fc6c2f14590ce78dcf5d8a8e4e1d2d
chagow <tshadmoua@gmail.com>
2025-01-30 14:47:08 -0600

    Create DebugOne3.cs

---
+++ DebugOne3.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+using System.Globalization;
+class DebugOne3
+{
+   static void Main()
+   {
+      Console.WriteLine("This program lists the number 1 to 5 vertically");
+      Console.WriteLine("1");
+      Console.WriteLine("2");
+      Console.WriteLine("3");
+      Console.WriteLine("4");
+      Console.WriteLine("5");
+   }
+}
