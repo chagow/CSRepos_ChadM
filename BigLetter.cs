7876bbef816d3e0abe05dd489b0cde04551af96a
chagow <tshadmoua@gmail.com>
2025-01-30 06:13:29 -0600

    Create BigLetter.cs

---
+++ BigLetter.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+using System.Globalization;
+class BigLetter
+{
+	static void Main()
+	{
+		Console.WriteLine("CCCCCCCC");
+		Console.WriteLine("C");
+		Console.WriteLine("C");
+		Console.WriteLine("C");
+		Console.WriteLine("C");
+		Console.WriteLine("CCCCCCCC");
+	}
+}
