13bc8e7d1f3046ef5f6c790673c7304fab93b823
chagow <tshadmoua@gmail.com>
2025-01-30 14:12:39 -0600

    Create BurmaShave.cs

---
+++ BurmaShave.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+using System.Globalization;
+class BurmaShave
+{
+	static void Main()
+	{
+		Console.WriteLine("Now can snore");
+		Console.WriteLine("Six more minutes");
+		Console.WriteLine("Than before");
+		Console.WriteLine("By using");
+		Console.WriteLine("Burma Shave");
+	}
+}
