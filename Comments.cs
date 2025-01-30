3894cb1a18ce71b9e24a5a29ed8997d6a1f59d73
chagow <tshadmoua@gmail.com>
2025-01-29 21:41:21 -0600

    Create Comments.cs

---
+++ Comments.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+using System.Globalization;
+class Comments
+{
+	static void Main()
+	{
+    /*
+    This is a block comment
+    */
+    // This is a line comment
+	        Console.WriteLine("This is a line.")
+	}
+}
