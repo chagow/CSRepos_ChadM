3338e672cf26b41fd1ffa3f0b0831177771f1568
chagow <tshadmoua@gmail.com>
2025-01-30 05:57:44 -0600

    Create StopSign.cs

---
+++ StopSign.cs
@@ -0,0 +1,17 @@
+using System;
+using static System.Console;
+using System.Globalization;
+class StopSign
+{
+	static void Main()
+	{
+		Console.WriteLine("   XXXXXX   ");
+		Console.WriteLine(" X        X");
+		Console.WriteLine("X   STOP   X");
+		Console.WriteLine(" X        X");
+		Console.WriteLine("   XXXXXX   ");
+		Console.WriteLine("      X");
+		Console.WriteLine("      X");
+		Console.WriteLine("      X");
+	}
+}
