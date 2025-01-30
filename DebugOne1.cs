3158ade2ac2273d71b2916313f8a5177ab8b3c1c
chagow <tshadmoua@gmail.com>
2025-01-30 14:24:41 -0600

    Create DebugOne1.cs

---
+++ DebugOne1.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+using System.Globalization;
+class DebugOne1
+{
+   static void Main()
+   {
+      Console.WriteLine("This program displays some keyboard punctuation");
+      Console.WriteLine("!   exclamation point");
+      Console.WriteLine("@   at-sign");
+      Console.WriteLine("#   pound sign or hash mark");
+      Console.WriteLine("$   dollar sign");
+   }
+}
