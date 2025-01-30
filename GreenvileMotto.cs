9d20311bf88cc6f90932f81327fcb1336ca53b7a
chagow <tshadmoua@gmail.com>
2025-01-30 15:31:02 -0600

    Create GreenvileMotto.cs

---
+++ GreenvileMotto.cs
@@ -0,0 +1,12 @@
+using System;
+using static System.Console;
+using System.Globalization;
+class GreenvilleMotto
+{
+   static void Main(string[] args)
+   {
+       Console.WriteLine("************************************");
+       Console.WriteLine("*  The stars shine in Greenville.  *");
+       Console.WriteLine("************************************");
+   }
+}
