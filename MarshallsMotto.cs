2130051f705014923a5affbd033ac83e1d44b536
chagow <tshadmoua@gmail.com>
2025-01-30 15:39:20 -0600

    Create MarshallsMotto.cs

---
+++ MarshallsMotto.cs
@@ -0,0 +1,12 @@
+using System;
+using static System.Console;
+using System.Globalization;
+class MarshallsMotto
+{
+   static void Main(string[] args)
+   {
+      Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+      Console.WriteLine("M  Make your vision your view.  M");
+      Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+   }
+}
