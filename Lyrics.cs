b2cdb77d2717f554cfbfa22c84afbf2d851534fc
chagow <tshadmoua@gmail.com>
2025-01-29 21:34:10 -0600

    Update Lyrics.cs

--- Lyrics.cs
+++ Lyrics.cs
@@ -5,9 +5,9 @@ class Lyrics
 {
 	static void Main()
 	{
-		  Console.WriteLine("They say there's no such thing as forever");
-    	Console.WriteLine("I try to smile and say that's fine too");
-    	Console.WriteLine("Things that shine will someday lose their light");
-    	Console.WriteLine("Until the flame goes on, we'll keep walking on");
+		Console.WriteLine("They say there's no such thing as forever");
+    		Console.WriteLine("I try to smile and say that's fine too");
+    		Console.WriteLine("Things that shine will someday lose their light");
+    		Console.WriteLine("Until the flame goes on, we'll keep walking on");
 	}
 }
