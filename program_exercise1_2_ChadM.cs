ab14e83ea80569055f13d384771ce93325417768
chagow <tshadmoua@gmail.com>
2025-01-29 21:31:36 -0600

    Create Lyrics.cs

---
+++ program_exercise1_2_ChadM.cs
@@ -0,0 +1,13 @@
+using System;
+using static System.Console;
+using System.Globalization;
+class Lyrics
+{
+	static void Main()
+	{
+		  Console.WriteLine("They say there's no such thing as forever");
+    	Console.WriteLine("I try to smile and say that's fine too");
+    	Console.WriteLine("Things that shine will someday lose their light");
+    	Console.WriteLine("Until the flame goes on, we'll keep walking on");
+	}
+}
